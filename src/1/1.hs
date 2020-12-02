4127007d46bb5efaf5fa9621faa3e88a6bee0991
Jacob Tan En <jacob.tan.en@gmail.com>
2020-12-02 17:20:06 +0800

    Day 1: add StackOverflow solution

--- src/1/1.hs
+++ src/1/1.hs
@@ -58,3 +58,30 @@ choose3 xs =
       (y, j) <- drop i xs `zip` [1 ..]
       z <- drop (i + j) xs
       return [x, y, z]
+
+
+-- | StackOverflow solution
+
+-- https://stackoverflow.com/questions/52602474/function-to-generate-the-unique-combinations-of-a-list-in-haskell/52605612
+
+-- Every set contains a unique empty subset.
+subsets 0 _ = [[]]
+
+-- Empty sets don't have any (non-empty) subsets.
+subsets _ [] = []
+
+-- Otherwise we're dealing with non-empty subsets of a non-empty set.
+-- If the first element of the set is x, we can get subsets of size n by either:
+--   - getting subsets of size n-1 of the remaining set xs and adding x to each of them
+--     (those are all subsets containing x), or
+--   - getting subsets of size n of the remaining set xs
+--     (those are all subsets not containing x)
+subsets n (x : xs) = map (x :) (subsets (n - 1) xs) ++ subsets n xs
+
+chooseN n xs = length $ subsets n xs
+
+-- >>> chooseN 2 [1..200]
+-- >>> chooseN 3 [1..200]
+-- 19900
+
+-- 1313400
