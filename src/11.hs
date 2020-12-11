e4a7a1bff40d7dd48b714f0fc5ce04ef134f0656
Jacob Tan En <jacob.tan.en@gmail.com>
2020-12-11 23:51:35 +0800

    Add to AdventOfCode2020: day 11: part 2: improved directions algorithm

--- src/11.hs
+++ src/11.hs
@@ -9,9 +9,9 @@ module Main where
 import Data.Function ((&))
 import Data.Functor ((<&>))
 import Data.Array ((!), Array, elems, listArray)
-import Data.List ((\\))
+import Data.List (delete)
 
-data Spot = O | E | F  -- Occupied | Empty | Floor
+data Spot = O | E | F | B -- Occupied | Empty | Floor | Border (imaginary)
   deriving (Show, Eq)
 
 type Grid = Array (Int, Int) Spot
@@ -22,10 +22,10 @@ main = do
   let n = length . head $ input0
       m = length input0
       -- pad borders empty cells (=floor)
-      input1 = replicate (n+2) '.'
-        : map ((++ ['.']) . ('.':)) input0
-        ++ [replicate (n+2) '.']
-      parseChar = \case '#' -> O; 'L' -> E; '.' -> F
+      input1 = replicate (n+2) 'B'
+        : map ((++ ['B']) . ('B':)) input0
+        ++ [replicate (n+2) 'B']
+      parseChar = \case '#' -> O; 'L' -> E; '.' -> F; 'B' -> B
                         _ -> error "unrecognised char"
       input2 = input1 & concatMap (map parseChar)
       input = listArray ((0,0),(m+1,n+1)) input2
@@ -34,13 +34,13 @@ main = do
   putStrLn "\n__Part 1"
   putStrLn "skipped to save time for part 2"
   -- print $ part1 (m,n) input
+
   putStrLn "\n__Part 2"
   print $ part2 (m,n) input
 
-adjacents1 (i,j) = [(i',j') | i' <- [i-1 .. i+1], j' <- [j-1 .. j+1]] \\ [(i,j)]
+adjacents1 (i,j) = delete (i,j) [(i',j') | i' <- [i-1 .. i+1], j' <- [j-1 .. j+1]]
 -- >>> adjacents1 (2,2)
 -- [(1,1),(1,2),(1,3),(2,1),(2,3),(3,1),(3,2),(3,3)]
-
 
 solve1 :: (Int, Int) -> Grid -> Grid
 solve1 (m,n) = go
@@ -59,6 +59,7 @@ solve1 (m,n) = go
         O -> if shouldOccupiedSwap coord then E else O
         E -> if shouldEmptySwap coord then O else E
         F -> F
+        B -> B
 -- >>> take 5 $ (,) <$> [0..2] <*> [0..2]
 -- [(0,0),(0,1),(0,2),(1,0),(1,1)]
 
@@ -85,19 +86,31 @@ solve2 (m,n) = go
         O -> if shouldOccupiedSwap coord then E else O
         E -> if shouldEmptySwap coord then O else E
         F -> F
+        B -> B
+      adjacents2 :: (Int, Int) -> [Spot]
       adjacents2 (i,j) = directions
         where
-          north = [(i',j) | i' <- reverse [1..(i-1)] ]
-          south = [(i',j) | i' <- [(i+1)..m] ]
-          west = [(i,j') | j' <- reverse [1..(j-1)] ]
-          east = [(i,j') | j' <- [(j+1)..n] ]
-          northE = [(i',j') | i' <- reverse [1..(i-1)] | j' <- [(j+1)..n] ]
-          southE = [(i',j') | i' <- [(i+1)..m] | j' <- [(j+1)..n] ]
-          southW = [(i',j') | i' <- [(i+1)..m] | j' <- reverse [1..(j-1)] ]
-          northW = [(i',j') | i' <- reverse [1..(i-1)] | j' <- reverse [1..(j-1)] ]
+          --- | first-draft; it worked but so ugly LOL
+          -- north = [(i',j) | i' <- reverse [1..(i-1)] ]
+          -- south = [(i',j) | i' <- [(i+1)..m] ]
+          -- west = [(i,j') | j' <- reverse [1..(j-1)] ]
+          -- east = [(i,j') | j' <- [(j+1)..n] ]
+          -- northE = [(i',j') | i' <- reverse [1..(i-1)] | j' <- [(j+1)..n] ]
+          -- southE = [(i',j') | i' <- [(i+1)..m] | j' <- [(j+1)..n] ]
+          -- southW = [(i',j') | i' <- [(i+1)..m] | j' <- reverse [1..(j-1)] ]
+          -- northW = [(i',j') | i' <- reverse [1..(i-1)] | j' <- reverse [1..(j-1)] ]
+          -- directions =
+          --   [north, south, west, east, northE, southE, southW, northW]
+          --   <&> foldr (\ij r -> case grid ! ij of F -> r; x -> x) F
           directions =
-            [north, south, west, east, northE, southE, southW, northW]
-            <&> foldr (\ij r -> case grid ! ij of F -> r; x -> x) F
+            let deltas = delete (0,0) $ (,) <$> [0,1,-1] <*> [0,1,-1]
+                findLineOfSight _delta@(di,dj) = tail $ iterate (\(x,y) -> (x+di,y+dj)) (i,j)
+                linesOfSight = map findLineOfSight deltas
+             in linesOfSight
+                <&> foldr (\ij r -> case grid ! ij of F -> r; x -> x) F
 
 part2 :: (Int, Int) -> Grid -> Int
 part2 gridSize grid0 = length . filter (==O) . elems $ solve2 gridSize grid0
+
+-- >>> delete (0,0) $ (,) <$> [0,1,-1] <*> [0,1,-1]
+-- [(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,0),(-1,1),(-1,-1)]
