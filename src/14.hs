1f98168f64e1bab68d13a1b0e77253edcf38473f
Jacob Tan En <jacob.tan.en@gmail.com>
2020-12-14 17:16:59 +0800

    AdventOfCode2020: day 14: starting part 2.

---
+++ src/14.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wincomplete-patterns #-}
+{-# OPTIONS_GHC -Wincomplete-uni-patterns #-}
+
+import Data.Function ((&))
+import Data.Functor ((<&>))
+-- import Control.Arrow ((>>>))
+import Text.Read (readMaybe)
+import Data.Foldable (Foldable(foldl'))
+import Data.Bits
+import Data.Word (Word64)
+import Control.Exception (assert)
+import Data.Map(Map)
+import qualified Data.Map as Map
+import qualified Debug.Trace as Debug
+import Numeric (showIntAtBase)
+import Data.Char (intToDigit)
+
+main :: IO ()
+main = do
+  input <- readFile "14.txt" <&> lines <&> map (parseLine . words)
+  print $ part1 input
+  
+  putStrLn "\n__Part 2"
+  input2 <- readFile "14.txt" <&> lines <&> map (parseLine2 . words)
+  print $ part2 input2
+
+
+data Instruction = IMask Mask | IMem Mem
+data Mask = Mask {m1 :: !Word64, m0 :: !Word64}
+
+showBin :: Word64 -> ShowS
+showBin = showIntAtBase 2 intToDigit
+instance Show Mask where
+  showsPrec _ Mask{..} = ("Mask\n" ++) . showBin m1 . ("\n"++) . showBin m0 . ("\n"++)
+
+data Mem = Mem {loc :: !Word64, val :: !Word64} deriving Show
+
+parseLine :: [String] -> Instruction
+parseLine [left, _, right] =
+  case left -- & (\x -> Debug.trace (show x) x)
+  of
+    "mask" ->
+      let
+        f m@Mask{..} (i,x) = case readMaybe @Int [x] of
+          Just 1 -> m{m1 = setBit m1 i}
+          Just 0 -> m{m0 = clearBit m0 i}
+          Nothing -> m
+          _ -> error "invalid mask"
+      in
+        IMask $ foldl' f Mask{m1 = zeroBits, m0 = maxBound} (right & reverse & zip [0..])
+    _ ->
+      let (mem,
+            tail -> init -> read @Word64 -> loc) = splitAt 3 left
+      in assert (mem == "mem") $
+        IMem $ Mem {loc = loc, val = read @Word64 right}
+parseLine xs = error $ "invalid line length: " ++ show (length xs)
+
+data ProgState = PS {mask :: Mask, memory :: Map Word64 Word64}
+
+runProg :: Foldable t => t Instruction -> ProgState
+runProg = foldl' f (PS (Mask zeroBits zeroBits) Map.empty)
+  where
+    f ps@PS{mask=Mask{..},..} = \case
+      IMask ma -> ps{mask = ma} -- & Debug.trace (show ma)
+      IMem Mem{..} ->
+        let result = val .&. m0 .|. m1 -- .&. infixl7, .|. infixl5
+        in ps{memory = Map.insert loc result memory }
+
+part1 :: Foldable t => t Instruction -> Word64
+part1 (runProg -> PS{..}) = sum memory
+
+
+-- Part 2
+
+parseLine2 :: [String] -> Instruction
+parseLine2 [left, _, right] =
+  case left -- & (\x -> Debug.trace (show x) x)
+  of
+    "mask" ->
+      let
+        f m@Mask{..} (i,x) = case readMaybe @Int [x] of
+          Just 1 -> m{m1 = setBit m1 i}
+          Just 0 -> m{m0 = clearBit m0 i}
+          Nothing -> m
+          _ -> error "invalid mask"
+      in
+        IMask $ foldl' f Mask{m1 = zeroBits, m0 = maxBound} (right & reverse & zip [0..])
+    _ ->
+      let (mem,
+            tail -> init -> read @Word64 -> loc) = splitAt 3 left
+      in assert (mem == "mem") $
+        IMem $ Mem {loc = loc, val = read @Word64 right}
+parseLine2 xs = error $ "invalid line length: " ++ show (length xs)
+
+runProg2 :: Foldable t => t Instruction -> ProgState
+runProg2 = foldl' f (PS (Mask zeroBits zeroBits) Map.empty)
+  where
+    f ps@PS{mask=Mask{..},..} = \case
+      IMask ma -> ps{mask = ma} -- & Debug.trace (show ma)
+      IMem Mem{..} ->
+        let result = val .&. m0 .|. m1 -- .&. infixl7, .|. infixl5
+        in ps{memory = Map.insert loc result memory }
+
+part2 :: Foldable t => t Instruction -> Word64
+part2 (runProg2 -> PS{..}) = sum memory
