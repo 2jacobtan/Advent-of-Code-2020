579415525a549a355d05c86bb8bef231d1f36995
Jacob Tan En <jacob.tan.en@gmail.com>
2020-12-17 01:15:33 +0800

    AdventOfCode2020: day 16: part2: done.

--- src/16.hs
+++ src/16.hs
@@ -1,7 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiWayIf #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -16,13 +12,24 @@ import Data.Void (Void)
 import Text.Megaparsec.Char (eol)
 import Text.Megaparsec.Char.Lexer (decimal)
 import Data.Function ((&))
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe,mapMaybe)
 import Data.Set (Set)
 import qualified Data.Set as S
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Data.List (sortOn, foldl')
+import qualified Debug.Trace as Debug
+import Data.Functor ((<&>))
+import qualified Data.IntMap as M
+import Data.Monoid (Product(getProduct,Product))
+
+dt t x = Debug.trace (t ++ ": " ++ show x) x
+dt' x = Debug.trace (show x) x
 
 main :: IO ()
 main = do
   inputSample <- readFile "16sample.txt"
+  inputSample2 <- readFile "16sample2.txt"
   inputRaw <- readFile "16.txt"
   -- parseTest rulesBlockP (pack inputRaw)
   -- parseTest (rulesBlockP *> yourTicketP) (pack inputRaw)
@@ -32,7 +39,11 @@ main = do
   print $ solve1 . parseInput' $ inputRaw
 
   putStrLn "\n__Part 2"
-  print $ solve2 . parseInput' $ inputSample
+  -- print $ solve2 . parseInput' $ inputSample2
+  -- print $ part2 . parseInput' $ inputSample2
+  -- print $ part2' . parseInput' $ inputSample2
+  print $ part2 . parseInput' $ inputRaw
+  print $ part2' . parseInput' $ inputRaw
 
 -- Parsing
 
@@ -97,10 +108,46 @@ solve1 Input{..} = nearbyTickets
 isValidLine :: (Foldable t1, Foldable t2) => t2 (a -> Bool) -> t1 a -> Bool
 isValidLine predRules line = all (meetSomeRule predRules) line
 
-
-
-solve2 Input{..} = nearbyTickets
+solve2 :: Input -> [[Int]]
+solve2 Input{..} = nearbyTickets  -- & dt "nearbyTickets"
   & filter (isValidLine (map ruleToPred rulesBlock)) & (yourTicket :)
-  & id
-  -- & foldl' f (S.fromList )
+  & foldl' (flip filterListCandi) candidature
+  where
+    rulePredsV :: Vector (Int -> Bool)
+    rulePredsV = V.fromList (map ruleToPred rulesBlock)
 
+    rulesLen = V.length rulePredsV :: Int
+
+    candidature :: [[Int]]
+    candidature = replicate rulesLen [0..(rulesLen-1)]
+
+    filterValCandi :: Int -> [Int] -> [Int]
+    filterValCandi val candi = filter (\i -> rulePredsV V.! i $ val) candi
+
+    filterListCandi :: [Int] -> [[Int]] -> [[Int]]
+    filterListCandi list candiList = zipWith filterValCandi list candiList
+      -- & dt "filterListCandi"
+
+-- | (column#, field#) means: column# maps to field# in the field list
+part2 :: (Num a, Enum a) => Input -> [(a, Set Int)]
+part2 input = foldr f (const []) input' S.empty
+  where
+    input' = solve2 input <&> S.fromAscList & zip [0..] & sortOn (S.size . snd)
+    f (i, candiList0) r assignedFields0 = (i, candiList1) : r assignedFields1
+      where
+        candiList1 = candiList0 S.\\ assignedFields0
+        assignedFields1 = assignedFields0 <> candiList0
+
+part2' :: Input -> Int
+part2' input@Input{..} =
+  getProduct $ foldMap (\j -> Product $ yourTicket !! j) relevantColumns
+  where
+    input' = part2 input  -- [(column#,[field#])]
+    
+    fieldToColMap = input' <&> (\(c,S.elems -> (f:_)) -> (f,c)) & M.fromList
+    
+    -- | field#s
+    relevantRules = zip [0..] rulesBlock & filter (isDeparture . fst . snd)
+    isDeparture x = take (length @[] "departure") x == "departure"
+    
+    relevantColumns = map (\(fst -> i) -> fieldToColMap M.! i) relevantRules
